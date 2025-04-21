cd0e3d48ed8af30ae5894db942a0af0c6b3145fe
ABCD123-GOLD <arena6663@gmail.com>
2025-04-21 17:25:20 +0100

    dependencies installed

--- templates.py
+++ templates.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name= "cnnClassifier"
+project_name= "Chicken_Disease_CLassification"
 
 list_of_files= [
     ".github/workflows/.gitkeep",
@@ -21,14 +21,25 @@ list_of_files= [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
-
-
-
-
-
-
-
-
+    "research/trials.ipynb",
+    "template/index.html"
 
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename= os.path.split(filepath)
+
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info("Creating directory: {filedir} for the file: {filename}")
+
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open (filepath, "w") as f:
+            pass
+        logging.info(f'Creating empty file:{filepath}')
+        
+    else:
+        logging.info(f'{filename} is already created ')
